173d63514a8627576b61f979568a111313c274b1
oldmanscode <34528836+oldmanscode@users.noreply.github.com>
2017-12-25 10:40:43 +0800

    Update step3.js

--- step3.js
+++ step3.js
@@ -123,7 +123,32 @@
 			return typeof obj === "object" || typeof obj === "function" ?
 				class2type[toString.call(obj)] || "object" :
 				typeof obj;
+		},
+
+		each: function(obj, callback) {
+			var length, i = 0;
+	
+			if (isArrayLike(obj)) {
+				length = obj.length;
+				for(; i < length; i++) {
+					if (callback.call(obj[i], i, obj[i] ) === false) {
+						break;
+					}
+				}
+			} else {
+				for(i in obj) {
+					if(callback.call(obj[i], i, obj[i] ) === false) {
+						break;
+					}
+				}
+			}
+	
+			return obj;
 		}
+	});
+
+	jQuery.each("Boolean Number String Function Array Date RegExp Object Error Symbol".split(" "), function(i, name) {
+		class2type[ "[object " + name + "]" ] = name.toLowerCase();
 	});
 
 	var init = jQuery.fn.init = function() {};
